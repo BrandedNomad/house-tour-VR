e526f4f3e84529296f6ec502ba07ed3492a4ac3e
BrandedNomad <info@brandednomad.com>
2021-04-07 13:11:12 +0700

    feat: add vrButton to panel

--- index.js
+++ index.js
@@ -1,28 +1,75 @@
 import React from 'react';
 import {
   AppRegistry,
+  Environment,
+  asset,
   StyleSheet,
   Text,
   View,
+  VrButton
 } from 'react-360';
 
+import house from './data/houseData'
+
+
 export default class HouseTourVR extends React.Component {
+
+  state = {
+    room: house.House.roomName,
+    info:house.House.info,
+    adjacentRooms: house.House.adjacentRooms
+  }
+
+  clickHandler(roomSelection){
+    this.setState({
+      room:house[`${roomSelection}`].roomName,
+      info:house[`${roomSelection}`].info,
+      adjacentRooms:house[`${roomSelection}`].adjacentRooms,
+    })
+
+    Environment.setBackgroundImage(asset(`./360_${house[`${roomSelection}`].img}`))
+  }
+
+  createRoomButtons(adjacentRooms){
+    let rooms = adjacentRooms;
+    let buttons = [];
+
+    rooms.map((room) =>{
+      buttons.push(
+          <VrButton
+              key={`${room}-button`}
+              onClick={()=>{
+                this.clickHandler(room)
+              }}
+          >
+            <Text style={{backgroundColor:'green'}}>{room}</Text>
+          </VrButton>
+      )
+    })
+
+    return buttons;
+  }
+
   render() {
+
+    console.log(this.state.adjacentRooms);
     return (
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
+          <Text>
+            Room Selection
           </Text>
+          <Text>
+            {this.state.room}
+          </Text>
+          {this.createRoomButtons(this.state.adjacentRooms)}
         </View>
         <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
+          <Text>
+            Room Info
           </Text>
-        </View>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
+          <Text>
+            {this.state.info}
           </Text>
         </View>
       </View>
@@ -36,7 +83,8 @@ const styles = StyleSheet.create({
     width: 1000,
     height: 600,
     backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
   greetingBox: {
@@ -44,10 +92,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#000000',
     borderColor: '#639dda',
     borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
+  }
 });
 
 AppRegistry.registerComponent('HouseTourVR', () => HouseTourVR);
